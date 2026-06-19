885a164915081d54f917b2986fca6bef5105f40a
Wilkin-Porter <23351wp@hvhs.school.nz>
2026-06-19 22:52:05 +1200

    Login working, working on details page

--- firebase/io.js
+++ firebase/io.js
@@ -1,29 +1,57 @@
 let logout;
-let fb_userInformation;
+let waitingForDetails = true;
+let globalUserInformation;
 let authenticationListener;
 
-function fb_authenticate() {
+function fb_login() {
     logout = false;
     authenticationListener = firebase.auth().onAuthStateChanged(fb_checkLoginState)
 }
 
-function fb_checkLoginState(_userInformation) {
+function fb_checkLoginState(localUserInformation) {
     if (logout == true) {
         return;
     }
-    if (_userInformation) {
-        firebase.database().ref('userdata').update({
-            [_userInformation['uid']]: {
-                userName: _userInformation['displayName'],
-                email: _userInformation['email'],
-                profileURL: _userInformation['photoURL']
-            }
-        });
-        fb_userInformation = _userInformation;
-        displayLoginInformation();
-        loginButtonDisplay('hide');
+    if (localUserInformation) {
+        globalUserInformation = localUserInformation;
+        firebase.database().ref('/userdata').child(globalUserInformation['uid']).once('value', fb_checkExistingData, fb_error);
     } else {
         fb_loginPopup();
+    }
+}
+
+function fb_checkExistingData(firebaseUserInformation) {
+    // If local name is different from the name stored in firebse, update the firebase name with the local name
+    if (firebaseUserInformation.val()['googleName'] != globalUserInformation['displayName']) {
+        firebase.database().ref('userdata/' + globalUserInformation['uid']).update({googleName: globalUserInformation['displayName']});
+        //console.log("updated name");
+    } else {
+        //console.log("didn't update name");
+    }
+
+    // If local email is different from the email stored in firebse, update the firebase email with the local email
+    if (firebaseUserInformation.val()['googleEmail'] != globalUserInformation['email']) {
+        firebase.database().ref('userdata/' + globalUserInformation['uid']).update({googleEmail: globalUserInformation['email']});
+        //console.log("updated email");
+    } else {
+        //console.log("didn't update email");
+    }
+
+    // If local profile URL is different from the profile URL stored in firebse, update the firebase profile URL with the local profile URL
+    if (firebaseUserInformation.val()['googleProfileURL'] != globalUserInformation['photoURL']) {
+        firebase.database().ref('userdata/' + globalUserInformation['uid']).update({googleProfileURL: globalUserInformation['photoURL']});
+        //console.log("updated profile url");
+    } else {
+        //console.log("didn't update profile email");
+    }
+
+    if (('formName' in firebaseUserInformation.val()) == true) {
+        console.log("form name exists");
+        displayLoginInformation(firebaseUserInformation.val()['formName'], firebaseUserInformation.val()['googleProfileURL']);
+        loginButtonDisplay('hide');
+    } else {
+        console.log("form name doesn't exist");
+        window.location.href = "details.html";
     }
 }
 
@@ -40,5 +68,19 @@ function fb_logout() {
 }
 
 function fb_error(){
-    // Don't forget your error handling!
+    console.error('something went wrong')
 }
+
+
+/*
+firebase.database().ref('userdata').update({
+    [localUserInformation['uid']]: {
+        userName: localUserInformation['displayName'],
+        email: localUserInformation['email'],
+        profileURL: localUserInformation['photoURL']
+    }
+});
+globalUserInformation = localUserInformation;
+displayLoginInformation();
+loginButtonDisplay('hide');
+*/
--- firebase/script.js
+++ firebase/script.js
@@ -4,17 +4,17 @@ const LOGOUT_BUTTON = document.getElementById("logoutButton");
 const PROFILE_IMAGE = document.getElementById("profileImage");
 loginButtonDisplay('show');
 
-function displayLoginInformation() {
-    if (fb_userInformation == null) {
+function displayLoginInformation(_formName, _googleProfileURL) {
+    if (globalUserInformation == null) {
         console.error('An error occured during sign in.');
         LOGIN_INFORMATION.style.color = 'red';
 		LOGIN_INFORMATION.textContent = 'An error occured during sign in. Please try again later, or contact the site administrator if you believe this is a mistake.';
     } else {
-        //console.log('Logged in as user: ' + fb_userInformation['displayName'] + '. Full user details:');
-        //console.log(fb_userInformation);
+        //console.log('Logged in as user: ' + globalUserInformation['displayName'] + '. Full user details:');
+        //console.log(globalUserInformation);
         LOGIN_INFORMATION.style.color = 'black';
-        LOGIN_INFORMATION.textContent = 'Logged in as user: ' + fb_userInformation['displayName'];
-        PROFILE_IMAGE.src = fb_userInformation['photoURL'];
+        LOGIN_INFORMATION.textContent = 'Logged in as user: ' + _formName;
+        PROFILE_IMAGE.src = _googleProfileURL;
         PROFILE_IMAGE.hidden = false;
     }
 }
